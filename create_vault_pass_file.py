832bb9448555c09a6a380a9ccf0c1201b4447436
Mogduz <gerald.leikam@aol.com>
2025-09-20 01:31:12 +0200

    Update create_vault_pass_file.py

--- create_vault_pass_file.py
+++ create_vault_pass_file.py
@@ -21,24 +21,7 @@ def check_path_arg(value: str) -> str:
     if isinstance(value, str):
         path: Path = Path(value)
         path: Path = path.resolve()
-        if not path.parent.exists():
-            create_parents: str = input(f'Parents: {path.parents} not exists. Do want to create it? Y/n: ')
-            if create_parents.lower() == 'y' or create_parents.lower() == '':
-                path.parent.mkdir(parents=True)
-            elif create_parents.lower() == 'n':
-                print(f'Path: {path} not found. exit')
-                exit(1)
-            else:
-                raise ValueError(f'input was false. abort')
-        if path.exists():
-            overwrite_file: str = input(f'File: {path} allready exists. Overwrite it? y/N: ')
-            if overwrite_file.lower() == 'y':
-                return path
-            elif overwrite_file.lower() == '' or 'n':
-                print('File will not overwritten. exit')
-                exit(2)
-            else:
-                raise ValueError(f'input was false. abort')
+        return Path
     else:
         raise argparse.ArgumentTypeError(f"'{value}' ist kein String")
 
@@ -53,9 +36,51 @@ def parse_args() -> argparse.Namespace:
 def generate_secret(length: int) -> str:
     return secrets.token_urlsafe(length)
 
+def check_path(path: Path, overwrite: bool = False, create_parents: bool = False) -> bool:
+    if not path.parent.exists():
+        if not create_parents:
+            create_parents_question: str = input(f'Parent directories: {path.parents} not exists. Do want to create it? Y/n: ').lower()
+            if len(create_parents_question) == 0 or len(create_parents_question) == 1:
+                match create_parents_question:
+                    case 'y':
+                        create_parents: bool = True
+                    case '':
+                        create_parents: bool = True
+                    case _:
+                        create_parents: bool = False
+            else:
+                create_parents: bool = False
+        if create_parents:
+            path.parent.mkdir(parents=True)
+    if path.parent.exists() and not path.exists():
+        file_ok: bool = True
+    if path.parent.exists() and path.exists():
+        if not overwrite:
+            overwrite_question: str = input(f'file: {path} allready exists. Overwrite? y/N: ').lower()
+            if len(overwrite_question) == 1 or len(overwrite_question) == 1:
+                match overwrite_question:
+                    case 'n':
+                        overwrite: bool = False
+                    case '':
+                        overwrite: bool = False
+                    case 'y':
+                        overwrite: bool = True
+            else:
+                overwrite: bool = False
+    if path.parent.exists() and path.exists() and overwrite:
+        file_ok: bool = True
+    if path.parent.exists() and path.exists() and not overwrite:
+        file_ok: bool = False
+    if path.parent.exists() and file_ok:
+        return True
+    return False
+
+def do_work(args: argparse.Namespace) -> None:
+    can_write: bool = check_path(path=args.path, overwrite=args.overwrite, create_parents=args.create_parents)
+    print(args)
+    print(can_write)
 
 if __name__ == '__main__':
-    args: argparse.Namespace = parse_args()
-    print(args)
+    do_work(args=parse_args())
     #file: Path = args.path
     #file.write_text(data=generate_secret(length=args.length))
